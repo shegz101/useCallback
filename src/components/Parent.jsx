a231c620d610c8a5dbd94507db5278d9f364a2ad
shegz101 <bobatesegun3@gmail.com>
2023-05-08 05:18:25 +0100

    cm

---
+++ src/components/Parent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Parent = () => {
+  return <div></div>;
+};
+
+export default Parent;
+
+int index = -1;
+inti = 0; 
+while(size > 0) 
+{ if(a[i] == key) { 
+  index = i; break; 
+} if(a[i] > key)) {
+   break; 
+  } i++; 
+} 
+return index;
